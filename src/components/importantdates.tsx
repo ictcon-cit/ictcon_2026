fa22cfe543723e16acefe1aa2e02dc3c9c0d25dd
Bhargav <u23cse1021@cit.ac.in>
2026-01-21 23:27:04 +0530

    Updated Dates

--- src/components/importantdates.tsx
+++ src/components/importantdates.tsx
@@ -10,8 +10,8 @@ const ImportantDates = () => {
             >
               Paper Submission starts
             </th>
-            <td className="md:px-6 md:py-4 px-2 py-1 font-bold">
-              15th August, 2025
+            <td className="md:px-6 md:py-4 px-2 py-1 font-bold  text-gray-900">
+              1st March, 2026
             </td>
           </tr>
           <tr className="bg-white border-black outline outline-black outline-1">
@@ -22,17 +22,18 @@ const ImportantDates = () => {
               Last Date of Paper Submission
             </th>
             <td className="md:px-6 md:py-4 px-2 py-1 font-bold">
-              <span className="line-through text-red-600">
-                15th September, 2025
+              <span className=" text-gray-900">
+                5th May, 2026
               </span>
               <br />
-              <span className="line-through text-red-600">
+             {/* <span className="line-through text-red-600">
                 30th September, 2025 (  Extended )
-              </span>
+              </span> */} 
               <br />
-              <span className="text-green-700">
+              {/*  <span className="text-green-700">
                 12th October, 2025 <span className="text-xs">(Hard Deadline)</span>
-              </span>
+              </span>  */} 
+             
             </td>
           </tr>
           <tr className="bg-white border-black outline outline-black outline-1">
@@ -43,9 +44,10 @@ const ImportantDates = () => {
               Notification of Acceptance
             </th>
             <td className="md:px-6 md:py-4 px-2 py-1 font-bold ">
-             <span className="line-through text-red-600"> 25th October, 2025</span>
+             <span className=" text-gray-900">10th July, 2026</span>
               <br />
-              <span className="text-green-700">31st October,2025</span>
+            {/* <span className="text-green-700">31st October,2025</span>  */}
+              
             </td>
           </tr>
           <tr className="bg-white border-black outline outline-black outline-1">
@@ -53,10 +55,10 @@ const ImportantDates = () => {
               scope="row"
               className="md:px-6 md:py-4 px-2 py-1 font-bold text-gray-900 md:whitespace-nowrap wrap"
             >
-              Registration
+            Author Registration
             </th>
-            <td className="md:px-6 md:py-4 px-2 py-1 font-bold">
-              15th November, 2025
+            <td className="md:px-6 md:py-4 px-2 py-1 font-bold  text-gray-900">
+              5th August, 2026
             </td>
           </tr>
           <tr className="bg-white border-black outline outline-black outline-1">
@@ -66,8 +68,8 @@ const ImportantDates = () => {
             >
               Date of Conference (Hybrid mode)
             </th>
-            <td className="md:px-6 md:py-4 px-2 py-1 font-bold">
-              2-3 December, 2025
+            <td className="md:px-6 md:py-4 px-2 py-1 font-bold  text-gray-900">
+              2-4 November, 2026
             </td>
           </tr>
         </tbody>
