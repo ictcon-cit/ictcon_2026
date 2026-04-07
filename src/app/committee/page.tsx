052fac8f87ca0d2ecd919f5bde40e7f78c36520f
ictcon-cit <ictcon2025@gmail.com>
2026-04-07 13:46:21 +0530

    Update page.tsx

--- src/app/committee/page.tsx
+++ src/app/committee/page.tsx
@@ -227,7 +227,7 @@ const CommitteePage = () => {
             <div>Dr. Anupam Kumar, NIT Patna</div>
             <div>Dr. Mohd Jawed Khan, GNIOT, Greater Noida</div>
             <div>Dr. Deep Singh, Dr. B. R. Ambedkar University Delhi</div>
-            <div>Dr. Sabyasachi Bhattacharyya, Barak Valley Engineering College, Assam</div>
+            <div>Dr. Sabyasachi Bhattacharyya, Assam Engineering College and Barak Valley Engineering College, Assam</div>
             <div>Prof. Manoj Gupta, SMVDU Jammu</div>
             <div>Dr. Vandana Bharti, IIT Ropar</div>
           </div>
