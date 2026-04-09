74d8d94b0eb730a129a291741fa4a7e17985f36b
ictcon-cit <ictcon2025@gmail.com>
2026-04-09 20:03:53 +0530

    Update page.tsx

--- src/app/venue/page.tsx
+++ src/app/venue/page.tsx
@@ -69,7 +69,7 @@ const SubmissionPage = () => {
               </h3>
 
               <p className="text-gray-600 mb-6 text-sm md:text-base text-left">
-                <span className="font-semibold">IIT Goa:</span> Located within the GEC (Goa Engineering College) Campus. Follow the signboards for "IIT Goa Administration" or the specific venue (e.g., LT-3). <br />
+                <span className="font-semibold text-red-600">IIT Goa:</span> Located within the GEC (Goa Engineering College) Campus. Follow the signboards for "IIT Goa Administration" or the specific venue (e.g., LT-3). <br />
                 <span className="font-semibold">NFSU Goa:</span> Located adjacent to the GEC campus at the Transit Campus. <br />
                 <span className="font-semibold">Landmark:</span> The campus is located very close to the historic Shivaji Statue and the Gopal Ganapati Temple at Farmagudi. <br />
                 <span className="font-semibold">Local Transport: </span>While buses are frequent during the day, we recommend booking a taxi or using the GoaMiles app for late-evening travel. <br />
