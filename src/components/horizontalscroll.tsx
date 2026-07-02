3d71437c4d1fdff581f871294897af9076935773
Bhargav <u23cse1021@cit.ac.in>
2026-07-02 10:31:37 +0530

    Updated home

--- src/components/horizontalscroll.tsx
+++ src/components/horizontalscroll.tsx
@@ -5,7 +5,7 @@ const HorizontalScroll: React.FC = () => {
     <p className="text-blue-500 text-sm md:text-lg whitespace-nowrap overflow-hidden text-ellipsis">
 
        <span className="ml-2 font-semibold text-blue-600">
-  The proceedings of the conference will be published by Scopus Indexed ‐ Springer in Communications in Computer and Information Science series (CCIS), (Approval Pending).
+  The proceedings of the conference will be published by Scopus Indexed ‐ Springer in Communications in Computer and Information Science series (CCIS), ( Final Acceptance of Springer CCIS Proceedings).
 </span>
 
       {/* <span className="font-bold text-red-500">
